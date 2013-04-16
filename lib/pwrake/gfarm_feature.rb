5c5b00ad125241b8103b135b201b287ef2647599
Masahiro TANAKA <masa16.tanaka@gmail.com>
2013-04-16 21:40:53 +0900

    new: GfarmQueue#after_check

--- lib/pwrake/gfarm_feature.rb
+++ lib/pwrake/gfarm_feature.rb
@@ -187,6 +187,24 @@ module Pwrake
 
   class GfarmQueue < LocalityAwareQueue
 
+    def after_check(tasks)
+      list = []
+      tasks.each do |t|
+	list << t.name if t.kind_of? Rake::FileTask
+      end
+      if !list.empty?
+	Log.info "-- after_check: size=#{list.size} #{list.inspect}"
+	gfwhere_result = GfarmPath.gfwhere(list)
+	tasks.each do |t|
+	  if t.kind_of? Rake::FileTask
+	    t.location = gfwhere_result[GfarmPath.local_to_fs(t.name)]
+	  end
+	end
+	#puts "'#{self.name}' exist? => #{File.exist?(self.name)} loc => #{loc}"
+      end
+    end
+
+=begin
     def abr_msg(a)
       m = a[0..5].map{|x| x}.inspect
       m.sub!(/]$/,",...") if a.size > 6
@@ -223,6 +241,7 @@ module Pwrake
       Log.info "-- GfarmQueue#where %.6fs %s" % [Time.now-start_time,abr_msg(filenames)]
       tasks
     end
+=end
 
   end
 
